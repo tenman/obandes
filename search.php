9e327b2880012ba775976c5fdfb24cc1087992b0
tenman <a.tenman@gmail.com>
2011-04-06 16:31:42 +0900

    new file:   images/bg-1.png     deleted:    images/d1-head.png     new file:   images/sidebar-title.png     new file:   images/title-bg.png     new file:   search.php

---
+++ search.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The no seach result for obandes.
+ *
+ * @package WordPress
+ * @subpackage obandes
+ * @since obandes 0.52
+ */
+?>
+<?php get_header();?>
+<?php
+if(WP_DEBUG == true){
+    echo '<!--'.basename(__FILE__,'.php').'['.basename(dirname(__FILE__)).']-->'."\n";
+}?>
+<section id="yui-main">
+<article id="post-0" class="post error404 not-found yui-b">
+  <h2 class="title h2">
+    <?php _e( 'Not Found', 'obandes' ); ?>
+  </h2>
+  <div class="content">
+    <p>
+      <?php _e( 'Apologies, but no results were found for the requested Archive. Perhaps searching will help find a related post.', 'obandes' ); ?>
+    </p>
+    <?php get_search_form(); ?>
+  </div>
+</article>
+<div class="clear"></div>	
+</section>
+<?php get_sidebar('1');?>
+<?php get_footer();?>
